070dd77f185e1bde94e29dd21a1eb6e5333dcd49
SQclassroom <sq.vivek@gmail.com>
2023-10-16 11:14:25 +0530

    16-10-2023

---
+++ JAVASCRIPT/1.Intro.variables/Variables/Lj-1.js
@@ -0,0 +1,73 @@
+/* JavaScript Variable */
+
+
+/*  Declaration Var() */
+
+
+
+/* Declaration Let() */
+
+
+
+/* Declaration Const() */
+
+
+
+/* string with quotes */
+
+{
+    // let x = "wor'ld";
+    // let y = 'wor"ld';
+    // let z = "wor\"ld";
+    // document.write(x + "<br>" + y + "<br>" + z);
+    // console.log(y + "<br>");
+}
+
+/*
+
+-> The latest ECMAScript(ES6) standard defines following data types: Out of which six data types are Primitive(predefined). 
+
+-> Boolean. true and false.
+
+-> null. A special keyword denoting a null value. (Because JavaScript is case-sensitive, null is not the same as Null, NULL, or any other variant.)
+
+-> undefined. A top-level property whose value is not defined.
+
+-> Number. An integer or floating point number. For example: 42 or 3.14159.
+
+-> BigInt. An integer with arbitrary precision. For example: 9007199254740992n.
+
+-> String. A sequence of characters that represent a text value. For example: "Howdy".
+
+-> Symbol. A data type whose instances are unique and immutable.
+
+*/
+
+
+/* types of variables */
+
+{
+    // var myInteger = 12; // 32-bit number (from -2,147,483,648 to 2,147,483,647)
+    // var myLong = 9310141419482; // 64-bit number (from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807)
+    // var myFloat = 5.5; // 32-bit floating-point number (decimal)
+    // var myDouble = 9310141419482.22; // 64-bit floating-point number
+    // var myBoolean = true; // 1-bit true/false (0 or 1)
+    // var myBoolean2 = false;
+    // var myNotANumber = NaN;
+    // var NaN_Example = 0/0; // NaN: Division by Zero is not possible
+    // var notDefined; // undefined: we didn't define it to anything yet
+    // window.alert(aRandomVariable); // undefined
+    // var myNull = null; // null
+    // etc...
+}
+
+
+
+
+
+
+
+         
+
+         
+            

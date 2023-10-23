c83d62ba2a3945e9f62c384a8dff6b6a5dba9870
SQclassroom <sq.vivek@gmail.com>
2023-10-23 11:06:45 +0530

    23-10-2023

--- 6.JAVASCRIPT/3.Convert.Statements/convert/convert.js
+++ 6.JAVASCRIPT/3.Convert.Statements/convert/convert.js
@@ -10,31 +10,81 @@ parseInt() Parse its argument and returns a whole number
 
 {
    // let n1 =  Number(true);
+   // console.log(n1);
+
    // let n2 =  Number(false);
+   // console.log(n2);
+
    // let n3 =  Number("10");	
+   // console.log(n3);
+   // console.log(typeof n3);
+
    // let n4 =  Number("  10");	
+   // console.log(n4);
+
    // let n5=  Number("10  ");
+   // console.log(n5);
+
    // let n6 =  Number(" 10  ");
+   // console.log(n6);
+
    // let n7 =  Number("10.33");	
+   // console.log(n7);
+   // console.log(typeof n7);
+
    // let n8 =  Number("10,33");	
+   // console.log(n8);
+   // console.log(typeof n8);
+
    // let n9 =  Number("10 33");	
-   // let n10 =  Number("John");	
+   // console.log(n9);
+
+   // let n10 =  Number("John");
+   // console.log(n10);	
+   // console.log(typeof n10);
 }
+
 {
-   // let pi1 =  parseInt("-10");		
+   // let pi1 =  parseInt("-10");	
+   // console.log(pi1);
+   // console.log(typeof pi1);	
+
    // let pi2 =  parseInt("-10.33");
+   // console.log(pi2);
+
    // let pi3 =  parseInt("10");
+   // console.log(pi3);
+
    // let pi4 =  parseInt("10.33");
+   // console.log(pi4);
+
    // let pi5 =  parseInt("10 20 30");	
+   // console.log(pi5);
+
    // let pi6 =  parseInt("10 years");	
+   // console.log(pi6);
+    
    // let pi7 =  parseInt("years 10");	
+   // console.log(pi7);
+   
 }
+
 {
-   // let pf1 =  parseFloat("10");		
-   // let pf2 =  parseFloat("10.33");	
-   // let pf3 =  parseFloat("10 20 30");	
+   // let pf1 =  parseFloat("10");
+   // console.log(pf1);	
+   // console.log(typeof pf1);
+
+   // let pf2 =  parseFloat("10.33");
+   // console.log(pf2);	
+
+   // let pf3 =  parseFloat("10 20 30");
+   // console.log(pf3);	
+
    // let pf4 =  parseFloat("10 years");	
+   // console.log(pf4);
+
    // let pf5 =  parseFloat("years 10");	
+   // console.log(pf5);
 }
 	
 
@@ -51,24 +101,55 @@ Number.parseInt() Convert a string to a whole number
 
 {
    // let method = Number.isInteger(10);	
+   // console.log(method);
+
    // let method1 = Number.isInteger(10.5);	
+   // console.log(method1);
       
    // let method2 = Number.isSafeInteger(10);	
-   // let method3 = Number.isSafeInteger(12345678901234567890);	
+   // console.log(method2);
+
+   // let method3 = Number.isSafeInteger(12345678901234567890);
+   // console.log(method3);	
    
-   // let method4 = Number.parseFloat("10");		
+   // let method4 = Number.parseFloat("10");	
+   // console.log(method4);
+   // console.log(typeof method4);
+
    // let method5 = Number.parseFloat("10.33");	
+   // console.log(method5);
+
    // let method6 = Number.parseFloat("10 20 30");	
+   // console.log(method6);
+
    // let method7 = Number.parseFloat("10 years");	
-   // let method8 = Number.parseFloat("years 10");	
+   // console.log(method7);
+
+   // let method8 = Number.parseFloat("years 10");
+   // console.log(method8);
+   // console.log(typeof method8);
+
    
-   // let method9 = Number.parseInt("-10");		
-   // let method10 = Number.parseInt("-10.33");		
-   // let method11 = Number.parseInt("10");			
-   // let method12 = Number.parseInt("10.33");		
-   // let method13 = Number.parseInt("10 6");		
-   // let method14 = Number.parseInt("10 years");		
-   // let method15 = Number.parseInt("years 10");		
+   let method9 = Number.parseInt("-10");	
+   console.log(method9);	
+
+   let method10 = Number.parseInt("-10.33");		
+   console.log(method10);
+
+   let method11 = Number.parseInt("10");	
+   console.log(method11);
+
+   let method12 = Number.parseInt("10.33");	
+   console.log(method12);	
+
+   let method13 = Number.parseInt("10 6");	
+   console.log(method13);
+
+   let method14 = Number.parseInt("10 years");	
+   console.log(method14);	
+
+   let method15 = Number.parseInt("years 10");
+   console.log(method15);		
 }
 
 
